a2c44551dedf930f2f477a24af12f41df7a00387
drdhaval2785 <drdhaval2785@gmail.com>
2025-03-25 12:15:08 +0530

    regen on 25 March 2025

--- scripts/redo_all.sh
+++ scripts/redo_all.sh
@@ -4,7 +4,7 @@ git pull origin master
 echo "UPDATING CSL-ORIG COMPLETED."
 echo ""
 cd ../csl-lnum/scripts
-dicts=(wil yat gst ben mw72 lan cae md mw shs ap90 mwe bor ae bur stc pwg gra pw ccs sch bop armh vcp skd inm vei pui bhs acc krm ieg snp pe pgn mci)
+dicts=(wil yat gst ben mw72 lan cae md mw shs ap90 mwe bor ae bur stc pwg gra pw ccs sch bop armh vcp skd inm vei pui bhs acc krm ieg snp pe pgn mci abch acph acsj pwkvn lrv fri)
 for dict in ${dicts[@]};
 do
 	echo "STARTED CONVERTING $dict";
